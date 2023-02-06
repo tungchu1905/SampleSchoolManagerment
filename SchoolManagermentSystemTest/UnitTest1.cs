354a50eaa3acbf66769c64a1822d8404ef7a91f6
tungchu1905 <chutrieutung2000@gmail.com>
2023-02-06 11:08:47 +0700

    add_test

---
+++ SchoolManagermentSystemTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SampleSchoolManagermentV1.Validation.FluentValidation;
+using FluentValidation.TestHelper;
+namespace SchoolManagermentSystemTest
+{
+    public class UnitTest1
+    {
+        private readonly ClassValidator _validator = new ClassValidator();
+
+        //[Fact]
+        //public void GivenAnInvalidClassNameValue_ShouldHaveValidationError()
+        //=> _validator.ShouldHaveChildValidator(model => model.ClassName, "Must Enter full");
+    }
+}
